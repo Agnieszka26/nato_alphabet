ad01ba6d3365492d705208ba6ad9b645cfd4326a
Agnieszka26 <medrek.agn@yahoo.com>
2025-03-17 08:25:38 +0100

    refactor: error handling

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pandas
 
-#TODO 1. Create a dictionary in this format:
 letters_frame = pandas.read_csv('nato_phonetic_alphabet.csv')
 dictionary = {row.letter:row.code for (index, row) in letters_frame.iterrows() }
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_word= input("What is your word to spell?\n")
-new_list_user_letters = [item.upper() for item in user_word]
-new_list_nato_words_user = [dictionary[l] for l in new_list_user_letters]
-
-print(new_list_nato_words_user)
+is_num = True
+while is_num:
+    user_word= input("What is your word to spell?\n")
+    try:
+        new_list_user_letters = [item.upper() for item in user_word]
+        new_list_nato_words_user = [dictionary[l] for l in new_list_user_letters]
+    except KeyError:
+        print("Only words and letters, please!")
+    else:
+        print(new_list_nato_words_user)
+        is_num=False
